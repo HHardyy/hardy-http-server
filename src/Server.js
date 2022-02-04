81ada6b221dae427e682e60cfb07c8b794ed7995
hardy <bingzhenmail@gmail.com>
2022-02-04 16:06:10 +0800

    '资源访问'

--- src/Server.js
+++ src/Server.js
@@ -2,7 +2,7 @@
  * @Author: 小方块 
  * @Date: 2022-02-04 13:13:37 
  * @Last Modified by: 小方块
- * @Last Modified time: 2022-02-04 14:02:43
+ * @Last Modified time: 2022-02-04 16:03:47
  * desc: 服务类
  */
 
@@ -11,8 +11,10 @@
 */
 const http = require('http')
 const fses = require('fs').promises
-const { createReadStream, createWriteStream } = require('fs')
+const { createReadStream, createWriteStream, readFileSync } = require('fs')
 const path = require('path')
+const url = require('url')
+const util = require('util')
 
 /**
  * dependencies
@@ -22,6 +24,8 @@ const debug = require('debug')('server-log')
 const mime = require('mime')
 const colors = require('colors')
 
+const template = readFileSync(path.resolve(__dirname, 'template.ejs'), 'utf-8')
+
 debug('server-log')
 
 class Server {
@@ -29,6 +33,7 @@ class Server {
     this.port = options.port
     this.directory = options.directory
     this.host = options.host
+    this.template = template
   }
   start() {
     const server = http.createServer(this._request.bind(this))
@@ -37,9 +42,49 @@ class Server {
       console.log(colors.green(`    http://${this.host}:${this.port}`))
     })
   }
-  _request(req, res) {
-    console.log('service running');
-    res.end('hello my project')
+  async _request(req, res) {
+    let { pathname } = url.parse(req.url, true)
+    pathname = decodeURIComponent(pathname)
+    const filePath = path.join(this.directory, pathname)
+    try {
+      const statObject = await fses.stat(filePath)
+      if (statObject.isFile()) {
+        this._sendFile(req, res, filePath, statObject)
+      } else {
+        let concatFilePath = path.join(filePath, 'index1.html')
+        try {
+          const statObject = await fses.stat(concatFilePath)
+          this._sendFile(req, res, concatFilePath, statObject)
+        } catch (err) {
+          this._sendDirs(req, res, filePath, statObject, pathname)
+        }
+      }
+    } catch (e) {
+      this._sendError(req, res, e)
+    }
+  }
+  _sendError(req, res, e) {
+    debug('debug: ', e)
+    res.statusCode = 404
+    res.end('Not Found')
+  }
+  _sendFile(req, res, filePath, statObject) {
+    res.setHeader('content-type', mime.getType(filePath) + ';charset=utf-8')
+    createReadStream(filePath).pipe(res)
+  }
+  async _sendDirs(req, res, filePath, statObject, pathname) {
+    try {
+      const dirs = (await fses.readdir(filePath)).map(dir => ({
+        dir,
+        href: path.join(pathname, dir)
+      }))
+
+      let templateStr = await ejs.render(this.template, { dirs }, { async: true })
+      res.setHeader('content-type', 'text/html;charset=utf-8')
+      res.end(templateStr)
+    } catch (e) {
+      this._sendError(req, res, e)
+    }
   }
 }
 
